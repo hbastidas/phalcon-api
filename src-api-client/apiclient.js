55a2a8b4e040dd66065b19f600819ee84d89d681
Heber Bastidas <heberbastidas@gmail.com>
2020-07-20 20:13:17 +0200

    creating cards from api

--- src-api-client/apiclient.js
+++ src-api-client/apiclient.js
@@ -4,5 +4,23 @@ window.$ = window.jQuery = $;
 
 (async function () {
   const episodes = await tinyRick.getEpisode()
-  console.log(episodes)
+
+
+  episodes.results.forEach((item, i) => {
+    $("<div>").addClass("col").addClass("s12").addClass("m4").append(
+      $("<div>").addClass("card").append(
+        $("<div>").addClass("card-image").addClass("waves-effect").addClass("waves-block").addClass("waves-light").append(
+          $("<img>").addClass("activator").attr("src","https://www.nosoloposters.com/16877-thickbox_default/vade-escolar-rick-morty.jpg")
+        ),
+        $("<div>").addClass("card-content").append(
+          $("<span>").addClass("card-title").addClass("activator").addClass("grey-text").addClass("text-darken-3").text(item.name).append($("<i>").addClass("material-icons").addClass("right").text("more_vert")),
+          $("<p>").append($("<a>").attr("href","#").text("this is link"))
+        ),
+        $("<div>").addClass("card-reveal").append(
+          $("<span>").addClass("card-title").addClass("grey-text").addClass("text-darken-4").text(item.name).append($("<i>").addClass("material-icons").addClass("right").text("close")),
+          $("<p>").text("info")
+        )
+      )
+    ).appendTo("#app")
+  });
 })();
